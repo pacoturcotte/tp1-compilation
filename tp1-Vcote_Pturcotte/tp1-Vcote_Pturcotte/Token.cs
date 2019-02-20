4ad92a5a5fbb09e217b0b65c02f8e9119a20fa2f
vcote331 <36552071+vcote331@users.noreply.github.com>
2019-02-20 10:23:37 -0500

    Commit sérieux pour un projet très sérieux.

---
+++ tp1-Vcote_Pturcotte/tp1-Vcote_Pturcotte/Token.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tp1_Vcote_Pturcotte
+{
+    public class Token
+    {
+        public TokenPosition Position{get;}
+
+        public string Type { get; }
+
+        public string Value { get; }
+
+        internal Token(string pType, string pValue, TokenPosition pPosition)
+        {
+            Type = pType;
+            Value = pValue;
+            Position = pPosition;
+        }
+
+
+        public override string ToString()
+        {
+            return "Token :" + Value + " Position : " + Position + " Type : " + Type;
+        }
+    }
+}
---
+++ tp1-Vcote_Pturcotte/tp1-Vcote_Pturcotte/Tokenizer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tp1_Vcote_Pturcotte
+{
+    class Tokenizer
+    {
+    }
+}
